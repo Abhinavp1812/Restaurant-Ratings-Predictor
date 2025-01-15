f5def1c1f6cb6a73d39e0d04668a4a83ffbb3269
Abhinav Prajapati <abhinavprajapati4444@gmail.com>
2025-01-15 21:56:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib
+st.set_page_config(layout ="wide")
+scaler = joblib.load("Scaler.pkl")
+
+st.title ("Restaurant Rating Prediction App")
+
+st.caption ("this app helps you to predict a restaurants review class")
+st.divider()
+
+averagecost = st.number_input("Please enter the estimated cost for two", min_value = 50, max_value = 999999, value = 1000, step = 200)
+tablebooking = st.selectbox("Restaurant has table booking?", ["Yes", "No"])
+onlinedelivery = st.selectbox("Restaurant has online booking?", ["Yes", "No"])
+pricerange = st.selectbox("What is the price range (1 Cheapest, 4 Most Expensive)", [1,2,3,4])
+
+predictbutton = st.button("Predict the review!")
+st.divider()
+model = joblib.load("mlmodel.pkl")
+bookingstatus = 1 if tablebooking == "Yes" else 0
+deliverystatus = 1 if onlinedelivery == "Yes" else 0
+
+values = [averagecost,bookingstatus,deliverystatus,pricerange]
+my_X_values = np.array(values).reshape(1,-1)
+X = scaler.transform(my_X_values)
+
+if predictbutton:
+    st.snow()
+    prediction = model.predict(X)
+    
+
+    if prediction<2.5:
+        st.write("Poor")
+    elif prediction <3.5:
+        st.write("Average")
+    elif prediction <4.0:
+        st.write("Good")
+    elif prediction < 4.5:
+        st.write("Very Good")
+    else:
+        st.write("Excellent")
